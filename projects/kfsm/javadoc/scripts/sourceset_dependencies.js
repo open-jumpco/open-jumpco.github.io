8516d6357afc4079f6a1f920272b88894e763489
corneil <corneil.duplessis@gmail.com>
2024-02-29 12:40:58 +0200

    Generated by gradle-git-publish.

--- projects/kfsm/javadoc/scripts/sourceset_dependencies.js
+++ projects/kfsm/javadoc/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"],":dokkaHtml/linuxMain":[":dokkaHtml/commonMain"]}'
+sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"],":dokkaHtml/linuxMain":[":dokkaHtml/commonMain"],":dokkaHtml/wasmMain":[":dokkaHtml/commonMain"]}'
